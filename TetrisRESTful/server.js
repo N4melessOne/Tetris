919b933349afbc1331fff0edf40cf63a4c25fbad
N4melessOne <gamerlacikaaa@gmail.com>
2023-01-14 23:09:52 +0100

    Add project files.

---
+++ TetrisRESTful/server.js
@@ -0,0 +1,7 @@
+'use strict';
+var http = require("http");
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+const { error } = require("console");
